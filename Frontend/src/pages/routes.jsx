5ea642d03add0769a10bb7725ac2633a8d458ecc
eramirez-2019031 <eramirez-2019031@kinal.edu.gt>
2024-04-26 10:40:14 -0600

    se agrego la ultima actualizacion

--- Frontend/src/pages/routes.jsx
+++
@@ -1,9 +0,0 @@
-import { DashboardPage } from "./dashboard";
-import { Auth } from "./auth";
- 
-const routes = [
-    { path: '/auth', element: <Auth/> },
-    { path: '/*', element: <DashboardPage/> }    
-]
- 
-export default routes;
